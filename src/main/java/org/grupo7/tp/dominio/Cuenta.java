0630e2d3f77f32ad8e2aba8f2cd32968929abb7c
Mariano Ingrassia <iimariano94@gmail.com>
2017-04-11 16:47:47 -0300

    Cambios en la clase Cuenta

--- src/main/java/org/grupo7/tp/dominio/Cuenta.java
+++ src/main/java/org/grupo7/tp/dominio/Cuenta.java
@@ -1,16 +1,41 @@
 package org.grupo7.tp.dominio;
 
+import java.util.Date;
+
 public class Cuenta {
 	
-	String	nombre;
+	String	nombreCuenta;
+	String	nombreEmpresa;
 	float	valor;
+	Date inicioPeriodo;
+	Date finPeriodo;
 	
-	public String getNombre() {
-		return nombre;
+
+	public String getNombreCuenta() {
+		return nombreCuenta;
 	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public void setNombreCuenta(String nombreCuenta) {
+		this.nombreCuenta = nombreCuenta;
 	}
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+	public Date getInicioPeriodo() {
+		return inicioPeriodo;
+	}
+	public void setInicioPeriodo(Date inicioPeriodo) {
+		this.inicioPeriodo = inicioPeriodo;
+	}
+	public Date getFinPeriodo() {
+		return finPeriodo;
+	}
+	public void setFinPeriodo(Date finPeriodo) {
+		this.finPeriodo = finPeriodo;
+	}
+	
 	public float getValor() {
 		return valor;
 	}
