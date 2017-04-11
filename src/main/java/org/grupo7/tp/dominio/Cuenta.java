38617f61a13c8cb92de91bbb4f65217a6cb2a338
Mariano Ingrassia <iimariano94@gmail.com>
2017-04-11 10:56:31 -0300

    Recreacion clase Cuenta

--- src/main/java/org/grupo7/tp/dominio/Cuenta.java
+++ src/main/java/org/grupo7/tp/dominio/Cuenta.java
@@ -5,6 +5,19 @@ public class Cuenta {
 	String	nombre;
 	float	valor;
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
 	
 	
 	
