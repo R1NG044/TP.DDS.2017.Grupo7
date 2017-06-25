ba0bb4d24bcd132c599c9bdd0a28fd6b1637cad5
Nadia <nadiagutierrez89@yahoo.com.ar>
2017-06-25 13:04:28 -0300

    Resources OK
    Se modifican Paths para que los test tomen los archivos desde Resources

--- src/test/java/ar/edu/utn/frba/dds/tp/test/TestAntlr.java
+++ src/test/java/ar/edu/utn/frba/dds/tp/test/TestAntlr.java
@@ -2,75 +2,74 @@ package ar.edu.utn.frba.dds.tp.test;
 //import org.antlr.runtime.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.Test;
-
 
 import dds.CalculadoraLexer;
 import dds.CalculadoraParser;
 import dds.ParserListener;
 
 public class TestAntlr {
-	
-	public static final String INPUT_PATH = "H:\\descargas\\DDS\\TP.DDS.2017.Grupo7\\archivosInput\\pruebaantlr3.txt";
-		
-	@SuppressWarnings("deprecation")
+
+	public static final String INPUT_PATH = "/pruebaantlr3.txt";
+
 	@Test
-	public void testFormulaAntlr() throws IOException{
-		
-        // ---- Esto es para evaluar una gramatica y crear expresiones -----------
-		
-		//ANTLRFileStream fs = new ANTLRFileStream(INPUT_PATH);
-		
-        
-		CalculadoraLexer lexer = new CalculadoraLexer(new ANTLRFileStream(INPUT_PATH));
-	       
-		 
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        CalculadoraParser parser = new CalculadoraParser(tokens);
-        CalculadoraParser.ExpresionContext expresionContext = parser.expresion();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        
-        ParserListener listener = new ParserListener();
-        walker.walk(listener, expresionContext);
+	public void testFormulaAntlr() throws IOException {
+		InputStream file = this.getInputFilePath();
 
-        //assertEquals(6, 6);
-        //assertEquals(6, listener.getExpresion().getResultado(), 0.01);
+		// ---- Esto es para evaluar una gramatica y crear expresiones
+		// -----------
+
+		CalculadoraLexer lexer = new CalculadoraLexer(CharStreams.fromStream(file));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		CalculadoraParser parser = new CalculadoraParser(tokens);
+		CalculadoraParser.ExpresionContext expresionContext = parser.expresion();
+		ParseTreeWalker walker = new ParseTreeWalker();
+
+		ParserListener listener = new ParserListener();
+		walker.walk(listener, expresionContext);
+
+		// assertEquals(6, 6);
+		// assertEquals(6, listener.getExpresion().getResultado(), 0.01);
 	}
-	
-//	@Test
-//	public void testGramatica() throws IOException{
-//		
-//        // ---- Esto es para evaluar una gramatica y crear expresiones -----------
-//		
-//		//ANTLRFileStream fs = new ANTLRFileStream(INPUT_PATH);
-//		
-//        CalculadoraLexer lexer = new CalculadoraLexer(new ANTLRFileStream(INPUT_PATH));
-//       
-// 
-//        CommonTokenStream tokens = new CommonTokenStream(lexer);
-//        CalculadoraParser parser = new CalculadoraParser(tokens);
-//        CalculadoraParser.ExpresionContext expresionContext = parser.expresion();
-//        ParseTreeWalker walker = new ParseTreeWalker();
-//        
-//        GramaticaListener listener = new GramaticaListener();
-//        walker.walk(listener, expresionContext);
-//
-//        assertEquals(6, 6);
-//        //assertEquals(6, listener.getExpresion().getResultado(), 0.01);
-//	}
-//	
-//	@Test
-//	public void testGramaticaFromString() throws IOException{
-//		//Invoco metodo Parser Helper
-//		
-//		ParserHelper.ParseExpresion("3*2");
-//		//ParserHelper.ParseExpresion("3+5+IND(INGRESO)*CUENTA(NETO)+3*2-5");
-//	}
-//	
 
+	// @Test
+	// public void testGramatica() throws IOException{
+	//
+	// // ---- Esto es para evaluar una gramatica y crear expresiones
+	// -----------
+	//
+	// //ANTLRFileStream fs = new ANTLRFileStream(INPUT_PATH);
+	//
+	// CalculadoraLexer lexer = new CalculadoraLexer(new
+	// ANTLRFileStream(INPUT_PATH));
+	//
+	//
+	// CommonTokenStream tokens = new CommonTokenStream(lexer);
+	// CalculadoraParser parser = new CalculadoraParser(tokens);
+	// CalculadoraParser.ExpresionContext expresionContext = parser.expresion();
+	// ParseTreeWalker walker = new ParseTreeWalker();
+	//
+	// GramaticaListener listener = new GramaticaListener();
+	// walker.walk(listener, expresionContext);
+	//
+	// assertEquals(6, 6);
+	// //assertEquals(6, listener.getExpresion().getResultado(), 0.01);
+	// }
+	//
+	// @Test
+	// public void testGramaticaFromString() throws IOException{
+	// //Invoco metodo Parser Helper
+	//
+	// ParserHelper.ParseExpresion("3*2");
+	// //ParserHelper.ParseExpresion("3+5+IND(INGRESO)*CUENTA(NETO)+3*2-5");
+	// }
+	//
+
+	private InputStream getInputFilePath() {
+		return this.getClass().getResourceAsStream(INPUT_PATH);
+
+	}
 }
-
-//CharStreams.fromPath(FileSystem.getPath(INPUT_PATH)
