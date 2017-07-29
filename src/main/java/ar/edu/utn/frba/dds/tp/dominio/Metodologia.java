ce34e8530a1271dcda9413e4e235b4b24e343e75
Brenda Stolarz <brenda.stolarz@gmail.com>
2017-07-29 12:57:30 -0300

    Commit Clase Metodologia

---
+++ src/main/java/ar/edu/utn/frba/dds/tp/dominio/Metodologia.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.tp.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Metodologia {
+	
+	private String nombre;
+	private List<Empresa> empresas = new ArrayList<Empresa>();
+	
+	
+
+}
