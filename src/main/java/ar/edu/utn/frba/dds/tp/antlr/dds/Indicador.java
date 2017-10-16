7fc055d80b95c364238029d9a361b391eb53b8c6
Nadia <nadiagutierrez89@yahoo.com.ar>
2017-10-16 18:41:46 -0300

    Fix para que al buscar Indicadores traiga los globales (que son para todos los users)
    Fix para que al buscar Indicadores traiga los globales (que son para todos los users)

--- src/main/java/ar/edu/utn/frba/dds/tp/antlr/dds/Indicador.java
+++ src/main/java/ar/edu/utn/frba/dds/tp/antlr/dds/Indicador.java
@@ -5,7 +5,7 @@ import ar.edu.utn.frba.dds.tp.dominio.Usuario;
 
 @Entity(name="Indicador")
 @Table(name = "indicador")
-@NamedQuery(name="buscarIndicadorPorUser",query="SELECT i FROM Indicador i WHERE i.usuario.id = :pIdUsuario")
+@NamedQuery(name="buscarIndicadorPorUser",query="SELECT i FROM Indicador i WHERE i.usuario.id = :pIdUsuario OR i.usuario.id = 0")
 public class Indicador implements IExpresion {
 	
 		
