070be558689e3a167c983fe461819d441f4c0de7
Nadia <nadiagutierrez89@yahoo.com.ar>
2017-11-26 18:21:29 -0300

    gitignore

--- src/main/java/ar/edu/utn/frba/dds/tp/dominio/IndicadorEmpresa.java
+++ src/main/java/ar/edu/utn/frba/dds/tp/dominio/IndicadorEmpresa.java
@@ -1,12 +1,22 @@
-package ar.edu.utn.frba.dds.tp.dominio;
 
-public class IndicadorEmpresa {
+package ar.edu.utn.frba.dds.tp.dominio;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import ar.edu.utn.frba.dds.tp.antlr.dds.*;
+
+
+//@Entity(name="IndicadorEmpresa")
+//@Table(name = "indicadorEmpresa")
+public class IndicadorEmpresa implements IExpresion{
 	
 	private String nombreEmpresa;
 	private double valorIndicador;
 	private Integer periodo;
 	
-	
+	public IndicadorEmpresa() {
+	}
 	
 	public IndicadorEmpresa(String nombreEmpresa, double valorIndicador, Integer periodo) {
 		super();
@@ -33,6 +43,30 @@ public class IndicadorEmpresa {
 	}
 	public void setPeriodo(Integer periodo) {
 		this.periodo = periodo;
+	}
+
+	@Override
+	public double calcularResultado(String empresa, Integer periodo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public IOperador getOperador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IExpresion getOperando1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IExpresion getOperando2() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
