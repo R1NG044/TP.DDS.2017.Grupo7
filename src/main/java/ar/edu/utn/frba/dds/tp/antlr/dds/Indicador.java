1f2ec901f4e10957e48a29e0caa1a98044dd81b5
Brenda Stolarz <brenda.stolarz@olx.com>
2017-11-27 00:35:39 -0300

    Se modifica id usuario general en query

--- src/main/java/ar/edu/utn/frba/dds/tp/antlr/dds/Indicador.java
+++ src/main/java/ar/edu/utn/frba/dds/tp/antlr/dds/Indicador.java
@@ -5,7 +5,7 @@ import ar.edu.utn.frba.dds.tp.dominio.Usuario;
 
 @Entity(name="Indicador")
 @Table(name = "indicador")
-@NamedQuery(name="buscarIndicadorPorUser",query="SELECT i FROM Indicador i WHERE i.usuario.id = :pIdUsuario OR i.usuario.id = 0")
+@NamedQuery(name="buscarIndicadorPorUser",query="SELECT i FROM Indicador i WHERE i.usuario.id = :pIdUsuario OR i.usuario.id = 1")
 public class Indicador implements IExpresion {
 	
 		
