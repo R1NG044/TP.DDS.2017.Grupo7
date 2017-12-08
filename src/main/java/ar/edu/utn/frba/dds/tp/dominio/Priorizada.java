e137d15753e58e60000825b0a80f27893467eaa1
Brenda Stolarz <brenda.stolarz@olx.com>
2017-12-08 11:52:10 -0300

    Agrego getters, setters y metodos de Priorizada. Empece logica de Taxativa

--- src/main/java/ar/edu/utn/frba/dds/tp/dominio/Priorizada.java
+++ src/main/java/ar/edu/utn/frba/dds/tp/dominio/Priorizada.java
@@ -1,5 +1,37 @@
 package ar.edu.utn.frba.dds.tp.dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.utn.frba.dds.tp.antlr.dds.Indicador;
+
 public class Priorizada {
 
+	Indicador indicador;
+	char orden;
+	
+	//--Getters y setters
+	public Indicador getIndicador() {
+		return indicador;
+	}
+	public void setIndicador(Indicador indicador) {
+		this.indicador = indicador;
+	}
+	public char getOrden() {
+		return orden;
+	}
+	public void setOrden(char orden) {
+		this.orden = orden;
+	}
+	
+	//--Metodos
+	
+	//Devuelve lista de empresas con su orden despues de calcular el indicador
+	public List<Empresa> aplicarMetodologia(List<Empresa> empresas, int periodo){
+		
+		List<Empresa> empresasQueCumplenConReglas = new ArrayList<Empresa>();
+		
+		return empresas;
+	
+	}
 }
--- src/main/java/ar/edu/utn/frba/dds/tp/dominio/Taxativa.java
+++ src/main/java/ar/edu/utn/frba/dds/tp/dominio/Taxativa.java
@@ -1,9 +1,68 @@
 package ar.edu.utn.frba.dds.tp.dominio;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Taxativa {
 
 	List<Regla> reglas;
+	String operacion;
+	
+	//--- Getters y setters
+	public List<Regla> getReglas() {
+		return reglas;
+	}
+
+	public void setReglas(List<Regla> reglas) {
+		this.reglas = reglas;
+	}
+
+	public String getOperacion() {
+		return operacion;
+	}
+
+	public void setOperacion(String operacion) {
+		this.operacion = operacion;
+	}
+
+	//--- Metodos ---
+	
+	//Devuelve las empresas que cumplen con las reglas segun el tipo de operacion AND/OR
+	public List<Empresa> aplicarMetodologia(List<Empresa> empresas, int periodo){
+		List<Empresa> empresasQueCumplenConReglas = new ArrayList<Empresa>();
+		
+		for(Empresa e: empresas){
+			if(operacion == "AND"){
+				boolean cumpleTodasLasReglas = true;
+				for (Regla r: this.reglas){
+					if (!evaluarMetodologiaAEmpresa(e, periodo)){
+						cumpleTodasLasReglas = false;
+						break;
+					}
+				}
+				
+			}
+			else{
+				boolean cumpleTodasLasReglas = false;
+				for (Regla r: this.reglas){
+					if (evaluarMetodologiaAEmpresa(e, periodo)){
+						cumpleTodasLasReglas = true;
+						break;
+					}
+				}
+			}
+			
+		}
+		
+		return empresasQueCumplenConReglas;
+		
+	}
+	
+	public boolean evaluarMetodologiaAEmpresa(Empresa empresa, int periodo){
+		
+		
+		return false;
+	}
+	
 	
 }
